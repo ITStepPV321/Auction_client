d1e160e1efb208d8865cf28fd954d7f13736318c
AuntL1ke <semenkos2005@gmail.com.comgit config --global user.email semenkos2005@gmail.com>
2024-12-04 20:15:31 +0000

    fixed push

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { getToken } from "../helpers/localStorage.helper";
 
-const urlAPI = "https://auctionwebapi-hacgcbg3btcnawct.polandcentral-01.azurewebsites.net/api"
+const urlAPI = "https://auctionclient2-cnguh0f5hzhfetey.polandcentral-01.azurewebsites.net/api"
+//"https://localhost:7017/api"
+//"https://auctionwebapi-hacgcbg3btcnawct.polandcentral-01.azurewebsites.net/api"
 // Пане Семен, введіть посилання на api, коли задеплоєте його
 
 export const instance = axios.create({
