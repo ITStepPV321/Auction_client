c51ee686435c7bf51a428e96924a14a600557b5e
Denys Gladan <69466074+MrLazYT@users.noreply.github.com>
2024-12-10 22:33:01 +0200

    Fixed Time Zone Problem

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { getToken } from "../helpers/localStorage.helper";
 
-const urlAPI = "https://localhost:7017/api"; //"https://auctionclient2-cnguh0f5hzhfetey.polandcentral-01.azurewebsites.net/api";
+const urlAPI = "https://auctionclient2-cnguh0f5hzhfetey.polandcentral-01.azurewebsites.net/api";
+//"https://localhost:7017/api";
 //"https://auctionwebapi-hacgcbg3btcnawct.polandcentral-01.azurewebsites.net/api"
 // Пане Семен, введіть посилання на api, коли задеплоєте його
 
